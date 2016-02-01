ee557ebbb5a8e6bf6c0d319543b412d1f2629239
charlesakio <charlesjavelona@gmail.com>
2016-02-01 15:34:25 -0500

    New project

---
+++ todoApp/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global todo-app, $*/
+
+
+window.todo-app = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function () {
+  'use strict';
+  todo-app.init();
+});
